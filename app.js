4a73c6e1ebe250a3f62c171d145707d84c5edec0
Sandali Tharuki <sandalitharuki95@gmail.com>
2020-02-11 20:04:54 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port= process.env.PORT ||3000;
+
+
+app.listen(port,function () {
+    console.log("listening to port"+port);
+});
+
+app.get('/', function (req, res) {
+    res.send('hello world')
+  })
+
+  app.get('/sandali', function (req, res) {
+    res.send('hello sandali')
+  })
+
+  app.get('/s', function (req, res) {
+    res.send('hello ')
+  })
