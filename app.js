7206500d0753c1bf2079866cb8021cf20369557d
Sandali Tharuki <sandalitharuki95@gmail.com>
2020-02-14 18:40:14 +0530

    respond send

--- app.js
+++ app.js
@@ -1,26 +1,22 @@
 const express = require('express');
 const app = express();
 const port= process.env.PORT ||3000;
+const path=require('path');
 
+app.use(express.static(path.join(__dirname,"path")));
 
 app.listen(port,function () {
-    console.log("listening to port"+port);
+    console.log("Listening to port " +port);
 });
 
-app.get('/', function (req, res) {
-    res.send('hello world')
-  })
+app.get('/',function(req,res){
+  res.send('Hello Sandali')
+})
 
-  app.get('/sandali', function (req, res) {
-    res.send('hello sandali')
-  })
+app.get('/users',function(req,res){
+  res.send('Hello Users')
+})
 
-  app.get('/s', function (req, res) {
-    res.send('hello ')
-  })
-
-  app.get('/a', function (req, res) {
-    res.send('hello ')
-  })
-
-  
+app.get('/sandali',function(req,res){
+  res.send('Hello Sooti')
+})
