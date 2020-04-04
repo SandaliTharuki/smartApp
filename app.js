cc8ed6203e53efec2dd350dc0c2d478d43a8a057
Sandali Tharuki <sandalitharuki95@gmail.com>
2020-04-04 21:31:51 +0530

    do

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const app = express();
-const port= process.env.PORT ||3000;
-const path=require('path');
+const port  = process.env.PORT ||3000;
+const path =require('path');
+const user = require('./routes/users');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const config = require('./config/database');
 
-app.use(express.static(path.join(__dirname,"path")));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+const connection = mongoose.connect(config.database);
+if(connection){
+  console.log("Database connected");
+}else {
+  console.log("Database not connected")
+}
+
+app.use(express.static(path.join(__dirname,"public")));
+
+app.use('/user',user);
 
 app.listen(port,function () {
     console.log("Listening to port " +port);
@@ -13,10 +30,4 @@ app.get('/',function(req,res){
   res.send('Hello Sandali')
 })
 
-app.get('/users',function(req,res){
-  res.send('Hello Users')
-})
 
-app.get('/sandali',function(req,res){
-  res.send('Hello Sooti')
-})
---
+++ routes/users.js
@@ -0,0 +1,29 @@
+
+
+
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
+
+router.post("/register",function(req,res){
+
+  const newUser = new User({
+
+    username:req.body.username,
+    name:req.body.name,
+    email:req.body.email,
+    password:req.body.password
+
+  });
+
+  
+     
+});
+
+router.get("",function(req,res){
+    res.send('Hello Users')
+  });
+
+
+  
+module.exports = router;
